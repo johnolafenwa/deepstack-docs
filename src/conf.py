04681ae5098d464b2faac421f7d94c4af25b5652
OlafenwaMoses <guymodscientist@gmail.com>
2020-11-30 23:30:06 +0100

    added icons to documentation

--- src/conf.py
+++ src/conf.py
@@ -52,3 +52,5 @@ html_theme = "sphinxawesome_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
+html_logo = "logo.png"
+html_favicon = "logo.png"
