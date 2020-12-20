a8d336a0795b0f138bef883a70427649aa474d45
Robin <robmarkcole@gmail.com>
2020-12-20 05:26:08 +0000

    Update conf.py

--- src/conf.py
+++ src/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, DeepQuest AI'
 author = 'DeepQuest AI'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.1'
+release = 'v1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
